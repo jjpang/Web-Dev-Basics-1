a9b7f65f143876e34d8218e18d5ecf4952e8f274
jjpang <jjpang21@gmail.com>
2021-08-25 21:28:45 -0700

    Add files via upload

---
+++ WebDevBasics1.3 - ivilon site (medium)/main.js
@@ -0,0 +1,8 @@
+// const image = document.querySelectorAll('.item-img')
+
+// image.forEach(function (i) {
+//     i.addEventListener("mouseover", function (e) {
+//         e.target.src = 'img/rod21.jpg'
+//         console.log('TEST')
+//     })
+// })
