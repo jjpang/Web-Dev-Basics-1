082fa2249d97cb60c0517d11ee88126a373428b7
jjpang <jjpang21@gmail.com>
2021-08-28 21:18:14 -0700

    Delete WebDevBasics1.3 - ivilon site (medium) directory

--- WebDevBasics1.3 - ivilon site (medium)/main.js
+++
@@ -1,8 +0,0 @@
-// const image = document.querySelectorAll('.item-img')
-
-// image.forEach(function (i) {
-//     i.addEventListener("mouseover", function (e) {
-//         e.target.src = 'img/rod21.jpg'
-//         console.log('TEST')
-//     })
-// })
